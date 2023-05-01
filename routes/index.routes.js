3047f24be0a3ace063f7e97a9c45339941d2d14f
Togara Hess <togarahess@gmail.com>
2023-05-01 17:09:46 +0200

    routes added

---
+++ routes/index.routes.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const router = express.Router();
+
+/* GET home page */
+router.get("/", (req, res, next) => {
+  res.render("index");
+});
+
+/* GET login page */
+router.get("/login.ejs", (req, res, next) => {
+  res.render("login");
+});
+
+/* GET assembly page */
+router.get("/assembly", (req, res, next) => {
+  res.render("assembly");
+});
+
+/* GET profile page */
+router.get("/profile", (req, res, next) => {
+  res.render("profile");
+});
+module.exports = router;
